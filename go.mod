2f916712dc1bd552976e043fc8f84cbfa09c98ce
r.halikov <r.halikov@npo-echelon.ru>
2026-01-16 15:37:40 +0300

    copy main

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/fitzplsr/sigstore-kms-yckms
+
+go 1.25.0
+
+require github.com/sigstore/sigstore v1.10.3
+
+require (
+	github.com/google/go-containerregistry v0.20.7 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.10.0 // indirect
+	github.com/sigstore/protobuf-specs v0.5.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/term v0.39.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260114163908-3f89685c29c3 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sigstore/sigstore/pkg/signature/kms/cliplugin/handler"
+)
+
+// See cliplugin.common.ProtocolVersion
+const expectedProtocolVersion = "v1"
+
+func main() {
+	// we log to stderr, not stdout. stdout is reserved for the plugin return value.
+	// spew.Fdump(os.Stderr, os.Args) // Useful for debugging
+	if protocolVersion := os.Args[1]; protocolVersion != expectedProtocolVersion {
+		err := fmt.Errorf("expected protocol version: %s, got %s", expectedProtocolVersion, protocolVersion)
+		handler.WriteErrorResponse(os.Stdout, err)
+		panic(err)
+	}
+
+	pluginArgs, err := handler.GetPluginArgs(os.Args)
+	if err != nil {
+		handler.WriteErrorResponse(os.Stdout, err)
+		panic(err)
+	}
+	// spew.Fdump(os.Stderr, pluginArgs) // Useful for debugging
+
+	//signerVerifier := &YandexKMSSignerVerifier{
+	//	hashFunc:      pluginArgs.InitOptions.HashFunc,
+	//	keyResourceID: pluginArgs.InitOptions.KeyResourceID,
+	//}
+
+	_, err = handler.Dispatch(os.Stdout, os.Stdin, pluginArgs, signerVerifier)
+	if err != nil {
+		// Dispatch() will have already called WriteResponse() with the error.
+		panic(err)
+	}
+	// spew.Fdump(os.Stderr, resp) // Useful for debugging
+}
